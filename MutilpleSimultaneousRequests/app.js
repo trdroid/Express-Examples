f4d8b2106ca333e83277111d63a1e40133b5f0ca
gruprog <gruprog@gmail.com>
2016-02-22 16:49:51 -0500

    snapshots

--- MutilpleSimultaneousRequests/app.js
+++ MutilpleSimultaneousRequests/app.js
@@ -1,64 +1,36 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
-app.use(function(req, res, next) {
-  res.send("Hello There!");
-});
+console.log("Server started ...");
+console.log("******************");
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var counter = 10;
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
+app.use(function(req, res, next) {  
+  console.log("Request for URL:" + req.url);
+  console.log("Waiting for " + counter + " minutes");
+  
+  if(counter == 0) {
+    counter = 10;  
+  } else {
+    counter--;
+  }
+    
+  setTimeout(function() {      
+      res.send("Hello World!");
+      console.log("Request has been served for " + req.url);
+   }, counter * 60 * 1000);
+ });
+ 
 module.exports = app;
+
+/*setTimeout(function() {
+  console.log("Hello World!");
+}, 60000);
+
+app.use(function(req, res, next) {  
+  console.log("Request for URL:" + req.url);
+  res.send("Hello There!");
+  console.log("Request has been served for " + req.url);
+});*/
